2eb5bbdaaef2669a1002c07ec6468cabf6ab35ca
Pratik1005 <intouchpratik94@gmail.com>
2018-12-28 23:05:04 -0800

    added a header

--- src/libs/ui-global/public_api.ts
+++ src/libs/ui-global/public_api.ts
@@ -2,4 +2,6 @@ export * from './footer/footer.component';
 export * from './sidebar/sidebar.component';
 export * from './siderail/siderail.component';
 export * from './toolbar/toolbar.component';
+export * from './action-bar/action-bar.component';
+export * from './header/header.component';
 export * from './global.module';
