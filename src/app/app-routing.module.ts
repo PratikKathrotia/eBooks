659aa4a54cadf5263c96581e390ecdcbd047d77b
Pratik1005 <intouchpratik94@gmail.com>
2019-01-06 13:48:57 -0800

    added sign-up component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,16 +2,21 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from '@angular-eBooks/ui-content';
+import { SignUpComponent } from './sign-up/sign-up.component';
 
 const routes: Routes = [
   {
     path: '',
-    redirectTo: '/home',
+    redirectTo: '/sign-up',
     pathMatch: 'full'
   },
   {
     path: 'home',
     component: HomeComponent
+  },
+  {
+    path: 'sign-up',
+    component: SignUpComponent
   }
 ];
 
