cfc9d18ab33314ee57b0b5c658e4b0bf910d5fb5
krunal90.patel@gmail.com <krunal90.patel@gmail.com>
2019-01-07 22:38:30 -0800

    Sign In Page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from '@angular-eBooks/ui-content';
+import { SignInComponent } from './sign-in/sign-in.component';
 
 const routes: Routes = [
   {
@@ -12,6 +13,10 @@ const routes: Routes = [
   {
     path: 'home',
     component: HomeComponent
+  },
+  {
+    path: 'sign-in',
+    component: SignInComponent
   }
 ];
 
