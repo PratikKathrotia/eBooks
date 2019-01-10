37b4aff04f5245623bcadb361fd9cb7c44e6dfbf
Pratik1005 <intouchpratik94@gmail.com>
2019-01-09 21:53:11 -0800

    changed cross field validator return condition

--- src/libs/sys-utils/form-validators/crossFieldErrorMatcher.ts
+++ src/libs/sys-utils/form-validators/crossFieldErrorMatcher.ts
@@ -7,6 +7,6 @@ import { ErrorStateMatcher } from '@angular/material';
 export class CrossFieldErrorMatcher implements ErrorStateMatcher {
   constructor(private errorStr: string) {}
   isErrorState(control: FormControl | null, form: FormGroupDirective): boolean {
-    return control.dirty && form.hasError(this.errorStr);
+    return (control.dirty || control.touched) && form.hasError(this.errorStr);
   }
 }
