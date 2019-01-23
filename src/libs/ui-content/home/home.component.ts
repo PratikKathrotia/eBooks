79d03ac3e859a5609c734f897cbb2f653870344c
Pratik1005 <intouchpratik94@gmail.com>
2019-01-22 21:05:44 -0800

    added basic things to grid book component

--- src/libs/ui-content/home/home.component.ts
+++ src/libs/ui-content/home/home.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, FormControl, Validator, FormArray } from '@angular/forms';
-import { BookService, IBook, IBookReview } from '@angular-eBooks/sys-utils';
+import { BookService, IBook } from '@angular-eBooks/sys-utils';
 
 @Component({
   selector: 'eb-home',
@@ -9,51 +8,14 @@ import { BookService, IBook, IBookReview } from '@angular-eBooks/sys-utils';
   providers: [ BookService ]
 })
 export class HomeComponent implements OnInit {
-  booksForm: FormGroup;
   allBooks: IBook[];
 
-  constructor(private bookService: BookService, private fb: FormBuilder) { }
+  constructor(private bookService: BookService) { }
 
   ngOnInit() {
-    this.booksForm = this.fb.group({
-      title: [''],
-      price: [''],
-      rating: [''],
-      author: [''],
-      description: [''],
-      publication: [''],
-      category: [''],
-      imageUrl: [''],
-      pages: [''],
-      reviews: this.fb.array([
-        this.fb.group({
-          personName: [''],
-          personRating: [''],
-          reviewDescription: ['']
-        })
-      ])
-    });
-
     // this.bookService.getBooks().subscribe(books => {
     //   this.allBooks = books;
     // });
-  }
-
-  get reviews() {
-    return this.booksForm.get('reviews') as FormArray;
-  }
-
-  addReview() {
-    this.reviews.push(this.fb.group({
-      personName: [''],
-      personRating: [''],
-      reviewDescription: ['']
-    }));
-  }
-
-  onSubmit() {
-    console.log(this.booksForm.value);
-    this.bookService.addBook(this.booksForm.value);
   }
 
 }
