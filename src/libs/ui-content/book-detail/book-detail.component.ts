44c9f6dbc49d35cee9942f348cb5f426cc6dca55
krunal90.patel@gmail.com <krunal90.patel@gmail.com>
2019-05-04 09:45:13 -0700

    added favorite to details page too

--- src/libs/ui-content/book-detail/book-detail.component.ts
+++ src/libs/ui-content/book-detail/book-detail.component.ts
@@ -1,25 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
-import { BookService, IBook, UtilService } from '@angular-eBooks/sys-utils';
+import { BookService, IBook, UtilService, UserService } from '@angular-eBooks/sys-utils';
 import { takeUntil } from 'rxjs/operators';
 import { Subject } from 'rxjs';
+import { User } from 'firebase';
 
 @Component({
   selector: 'eb-book-detail',
   templateUrl: './book-detail.component.html',
   styleUrls: ['./book-detail.component.scss']
 })
-export class BookDetailComponent implements OnInit {
+export class BookDetailComponent implements OnInit, OnDestroy {
   subject: Subject<any>;
   favorite: boolean;
   book: IBook | any;
   more_Review: boolean;
+  userId = localStorage.getItem('current_User');
+  user;
 
   constructor(
     private router: Router,
     private activatedRoute: ActivatedRoute,
     private bookService: BookService,
-    private utilService: UtilService
+    private utilService: UtilService,
+    private userService: UserService
   ) {
     this.utilService.toggleCustomerReview.subscribe(bool => this.more_Review = bool);
   }
@@ -33,13 +37,37 @@ export class BookDetailComponent implements OnInit {
       this.book = comingBook.data();
       this.utilService.sendLoadingIndicator(false);
     });
-    this.favorite = true;
+    if (this.userId) {
+      this.userService.getIndividualUser(this.userId).pipe(takeUntil(this.subject)).
+      subscribe(comingUser => {
+        this.user = comingUser.data();
+        if (this.user.favorites.includes(this.activatedRoute.snapshot.params['id'])) {
+            this.favorite = false;
+        } else {
+            this.favorite = true;
+        }
+      });
+    }
   }
   GetCustomerReview() {
     return this.book.reviews;
   }
   addFavorite() {
     this.favorite = !this.favorite;
+    this.utilService.showSnackBar(
+      this.favorite ? `${this.book.title} removed from your favorites.` :
+          `${this.book.title} added to your favorites`
+    );
+    if (!this.favorite) {
+      this.user['favorites'].push(this.activatedRoute.snapshot.params['id']);
+    } else {
+      this.user['favorites'].pop();
+    }
   }
 
+  ngOnDestroy() {
+    if (this.user) {
+      this.userService.setUser(this.user);
+    }
+  }
 }
